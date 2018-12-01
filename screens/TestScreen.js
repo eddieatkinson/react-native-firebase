8bdee89f259f21ec1c51f5bfad754aab2142a05b
Eddie Atkinson <eddiebatkinson@gmail.com>
2018-11-30 23:16:55 -0500

    init commit

---
+++ screens/TestScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { TestComponent } from './../components/AppComponents';
+
+export default class TestScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  render() {
+    return (
+      <View style={{ paddingTop: 20 }}>
+        <TestComponent />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  
+});
