c882caa084e90d2cd06ddebbb8671b44bc651197
Eddie Atkinson <eddiebatkinson@gmail.com>
2018-12-03 19:41:30 -0500

    verification

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,4 +1,5 @@
 import * as firebase from 'firebase';
+import { includes } from 'lodash';
 
 export function initializeApp() {
   if (!firebase.apps.length) {
@@ -14,14 +15,27 @@ export function createUser(email, password) {
   firebase.auth().createUserWithEmailAndPassword(email, password)
     .then(response => console.log(response))
     .catch((error) => {
-      // if (error)
-      console.log(error)
+      if (includes(error.toString(), 'The email address is badly formatted')) {
+        alert('Please provide a valid email.');
+      } else {
+        alert(error.toString());
+      }
     });
 }
 
 export function loginUser(email, password) {
   firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((response) => {
-      console.log(response);
+    .then(response => console.log(response))
+    .catch((error) => {
+      if (includes(error.toString(), 'The email address is badly formatted')) {
+        alert('Please provide a valid email.');
+      } else if (includes(error.toString(), 'The password is invalid or the user does not have a password')) {
+        alert('Your password is incorrect.');
+      } else if (includes(error.toString(), 'There is no user record corresponding to this identifier')){
+        alert('There is no user associated with this email.');
+      } else {
+        alert(error.toString());
+        console.log(error.toString());
+      }
     });
 }
